6e8675b001c0ae6c8b2b322e75b6423f9455f53a
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-10-15 22:50:36 -0300

    api
    api

--- API/src/main/java/io/github/fernandoschimiidt/clientes/ClientesApplication.java
+++ API/src/main/java/io/github/fernandoschimiidt/clientes/ClientesApplication.java
@@ -1,10 +1,23 @@
 package io.github.fernandoschimiidt.clientes;
 
+import io.github.fernandoschimiidt.clientes.model.entity.Cliente;
+import io.github.fernandoschimiidt.clientes.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClientesApplication {
+
+    @Bean
+    public CommandLineRunner run(@Autowired ClienteRepository repository){
+        return args -> {
+            Cliente cliente = Cliente.builder().cpf("43426192888").nome("Fernando").build();
+            repository.save(cliente);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ClientesApplication.class, args);
---
+++ API/src/main/java/io/github/fernandoschimiidt/clientes/rest/ClienteController.java
@@ -0,0 +1,59 @@
+package io.github.fernandoschimiidt.clientes.rest;
+
+import io.github.fernandoschimiidt.clientes.model.entity.Cliente;
+import io.github.fernandoschimiidt.clientes.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    private final ClienteRepository repository;
+
+    @Autowired
+    public ClienteController(ClienteRepository repository) {
+        this.repository = repository;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvar(@RequestBody @Valid Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+    @GetMapping("{id}")
+    public Cliente acharPorId(@PathVariable Integer id) {
+        return repository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Integer id) {
+        repository
+                .findById(id)
+                .map(cliente -> {
+                    repository.delete(cliente);
+                    return Void.TYPE;
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable Integer id, @RequestBody @Valid Cliente clienteAtt) {
+        repository
+                .findById(id)
+                .map(cliente -> {
+                    clienteAtt.setId(cliente.getId());
+                    return repository.save(clienteAtt);
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+}
