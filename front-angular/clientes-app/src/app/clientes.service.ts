428751425e1b08421b408467e91ccb28092103ed
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-10-18 22:40:37 -0300

    front

---
+++ front-angular/clientes-app/src/app/clientes.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from './clientes/Cliente';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientesService {
+
+  constructor(private http:HttpClient) {
+
+   }
+
+   salvar(cliente:Cliente):Observable<Cliente>{
+    return this.http.post<Cliente>('http://localhost:8080/api/clientes',cliente);
+   }
+
+
+}
