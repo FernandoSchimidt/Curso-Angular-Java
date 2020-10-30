7e2a1d6ed585965c361d0bfad6de13c4d1b22c9b
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-10-29 23:09:08 -0300

    cadastro de servico

--- front-angular/clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
+++ front-angular/clientes-app/src/app/servico-prestado/servico-prestado-form/servico-prestado-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from 'src/app/clientes/Cliente';
+import { ServicoPrestadoService } from 'src/app/servico-prestado.service';
 import { ClientesService } from '../../clientes.service'
 import { ServicoPrestado } from '../servicoPrestado';
 @Component({
@@ -11,9 +12,13 @@ export class ServicoPrestadoFormComponent implements OnInit {
 
   clientes: Cliente[]
   servico: ServicoPrestado
+  sucesso: boolean = false;
+  errors: string[];
+  id: number;
 
   constructor(
     private clienteService: ClientesService,
+    private servicoPrestadoService: ServicoPrestadoService
   ) {
     this.servico = new ServicoPrestado()
   }
@@ -24,7 +29,20 @@ export class ServicoPrestadoFormComponent implements OnInit {
       .subscribe(res => this.clientes = res)
   }
   onSubmit() {
-    console.log(this.servico)
-  }
+    if (this.id) {
 
+    }
+    else {
+      this.servicoPrestadoService
+        .salvar(this.servico)
+        .subscribe(res => {
+          this.sucesso = true;
+          this.errors = null;
+          this.servico = new ServicoPrestado;
+        }, errosResponse => {
+          this.sucesso = false;
+          this.errors = errosResponse.error.errors;
+        })
+    }
+  }
 }
