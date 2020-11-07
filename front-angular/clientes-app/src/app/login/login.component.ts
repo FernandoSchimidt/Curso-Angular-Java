2b5e2405584b46471f77ff75fb02b7dc27a11964
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2020-11-07 01:09:09 -0300

    logiin

--- front-angular/clientes-app/src/app/login/login.component.ts
+++ front-angular/clientes-app/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { Usuario } from './usuario';
+
 
 @Component({
   selector: 'app-login',
@@ -10,9 +13,13 @@ export class LoginComponent {
 
   username: string;
   password: string;
-  loginError: boolean;
-  cadastrando: boolean
-  constructor(private router: Router) { }
+  password2: string;
+  mensagemSucesso: string;
+  mensagemSenha: string;
+  cadastrando: boolean;
+  errors:String[];
+  constructor(private router: Router,
+    private authService: AuthService) { }
 
   onSubmit() {
     this.router.navigate(['/home'])
@@ -24,6 +31,39 @@ export class LoginComponent {
   }
   cancelaCadastro() {
     this.cadastrando = false;
+    this.errors = null
+    this.mensagemSenha = null
+  }
+
+  cadastrar() {
+    const usuario: Usuario = new Usuario();
+    if (this.password === this.password2) {
+      usuario.username = this.username;
+      usuario.password = this.password;
+
+      this.authService.salvarUsuario(usuario)
+        .subscribe(res => {
+          this.posCadastro()
+        }, errorResponse => {
+          {
+            this.mensagemSucesso = null
+            this.errors = errorResponse.error.errors;
+          }
+        })
+    } else {
+      this.mensagemSenha = "As Senhas são diferentes"
+      this.mensagemSucesso = null
+    }
+  }
+
+  logar() {
+
+  }
+
+  posCadastro() {
+    this.mensagemSenha = null
+    this.mensagemSucesso = "cadastro realizado com sucesso! Efetue login."
+    this.cadastrando = false
   }
 
 }
